ac7c0b87aa518807132c740f72ad33e38d871a61
KishanExcel <kishan.excel2011@gmail.com>
2023-11-07 18:26:42 +0530

    worked on ui of zappychat of audit section

--- frontend/src/Pages/MngUsers.jsx
+++ frontend/src/Pages/MngUsers.jsx
@@ -17,30 +17,30 @@ const MngUsers = () => {
     <div>
       <div className="p-6">
         <div className="flex justify-between">
-          <h6 className="font-bold  text-[#344767] text-3xl">Agency Users</h6>
-          <button onClick={handleOpenModal} className="bg-[#0F45F5] flex py-4 px-4 rounded-lg gap-2">
+          <h6 className="font-bold  text-[#344767] text-2xl">Agency Users</h6>
+          <button onClick={handleOpenModal} className="bg-[#0F45F5] flex py-2 px-4 rounded-lg gap-2">
             <i className="text-slate-50 text-xl">
               <AiOutlinePlus />
             </i>
-            <p className="mt-[-2%] text-xl text-slate-50 font-semibold">
+            <p className="mt-[-2%] text-md text-slate-50 font-semibold">
               Add New Users
             </p>
           </button>
         </div>
         <div className="w-full mt-6 bg-slate-50 shadow-lg  bg-opacity-17 rounded-lg">
-          <table className="w-full border-collapse">
+          <table className="w-full border-collapse rounded-lg">
             <thead>
-              <tr className="bg-blue-100 ">
-                <th className="py-2 font-medium text-[#8392B3]">USER REFERNCE</th>
-                <th className="py-2 font-medium text-[#8392B3]">
+              <tr className="bg-[#F3F5FE] ">
+                <th className="py-2 font-normal text-[#8392AB]">USER REFERNCE</th>
+                <th className="py-2 font-normal text-[#8392AB]">
                   EMAIL REFERNCE
                 </th>
-                <th className="py-2 font-medium text-[#8392B3]">ACTIONS</th>
+                <th className="py-2 font-normal text-[#8392AB]">ACTIONS</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="py-4 px-4 bg-slate-50 text-xl text-center" colSpan="3">
+                <td className="py-2  text-[#8392AB] bg-slate-50 text-lg text-center" colSpan="3">
                   No User found
                 </td>
               </tr>
