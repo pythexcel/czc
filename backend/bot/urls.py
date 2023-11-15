42f0487f84507756afc7eeb1b50b38190e2d36b5
pythexcel <aayush.excel2011@gmail.com>
2023-11-15 18:09:22 +0530

    clone api name change

--- backend/bot/urls.py
+++ backend/bot/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("bot", CreateBotAPI.as_view()),
     path("bot/<id>", CreateBotAPI.as_view()),
-    path("bot/clone_data/", CloneBotAPI.as_view()),
+    path("bot/clone/", CloneBotAPI.as_view()),
 ]
